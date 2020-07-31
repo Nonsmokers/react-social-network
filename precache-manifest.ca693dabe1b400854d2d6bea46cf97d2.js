411f834fb8f980e2e0076066f14d258a50c43707
Nonsmokers <Vasyuxnevichs@mail.ru>
2020-07-31 11:15:05 +0300

    Updates

---
+++ precache-manifest.ca693dabe1b400854d2d6bea46cf97d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72bb634e68cfc3cb90127796046d5ab",
+    "url": "/react_project_social_network/index.html"
+  },
+  {
+    "revision": "3aef1d3682b3500e3998",
+    "url": "/react_project_social_network/static/css/main.621ea55e.chunk.css"
+  },
+  {
+    "revision": "63cc91b66e5b92832002",
+    "url": "/react_project_social_network/static/js/2.52caf23b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react_project_social_network/static/js/2.52caf23b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aef1d3682b3500e3998",
+    "url": "/react_project_social_network/static/js/main.b6216be2.chunk.js"
+  },
+  {
+    "revision": "d02df783d33a9b17f737",
+    "url": "/react_project_social_network/static/js/runtime-main.efa3cf4c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react_project_social_network/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "0e1c8a48afb73886d9f44ec710ec7153",
+    "url": "/react_project_social_network/static/media/user-icon.0e1c8a48.png"
+  }
+]);
